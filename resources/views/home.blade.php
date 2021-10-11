bb1614ec0b63d0adf9533a85b29548ee476eb7eb
diasdigital <78405123+diasdigital@users.noreply.github.com>
2021-10-11 18:59:23 +0700

    Blade Templating Engine
    Di video ini kita mencoba templating engine yang dimiliki oleh blade, kemudian melakukan layouting pada views agar tidak melakukan banyak pengulangan
    Kita juga mencoba mengirimkan data postingan penuh lewat routes yang kemudian ditampilkan pada halaman posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Mina Blog | Home</title>
-</head>
-<body>
-    
-    <h1>Halaman Home</h1>
+@extends('layouts.main')
 
-    <script src="js/script.js"></script>
-</body>
-</html>
+@section('konten')
+    <h1>Halaman Home</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Myoui Mina",
         "email" => "mina@twice.com",
         "image" => "mina.jpg"
@@ -26,5 +29,52 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_post = [
+        [
+            "title" => "Post pertama",
+            "slug" => "post-pertama",
+            "author" => "Myoui Mina",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum tincidunt massa eu lobortis. Morbi semper metus erat, vel eleifend urna porttitor nec. Donec lacinia odio eu quam vehicula elementum. Proin nunc purus, ultricies in lectus ut, congue rutrum eros. Sed quis tortor eu quam molestie eleifend ut tristique felis. Pellentesque non mi aliquet, viverra leo in, elementum erat. Morbi volutpat nisl id felis pellentesque, sit amet sodales orci tristique. Fusce sed accumsan odio. Praesent vel mi nunc. Nam euismod."
+        ],
+        [
+            "title" => "Post Kedua",
+            "slug" => "post-kedua",
+            "author" => "Hirai Momo",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus augue dolor, et feugiat justo commodo vel. In at augue in elit vehicula laoreet porta vitae odio. Mauris ultrices vestibulum est sed tempus. Fusce suscipit ipsum quis erat eleifend, pretium accumsan nulla faucibus. Duis congue faucibus volutpat. Sed at tortor tempor, imperdiet leo ut, semper lacus. Pellentesque suscipit enim at."
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_post
+    ]);
+});
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_post = [
+        [
+            "title" => "Post pertama",
+            "slug" => "post-pertama",
+            "author" => "Myoui Mina",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elementum tincidunt massa eu lobortis. Morbi semper metus erat, vel eleifend urna porttitor nec. Donec lacinia odio eu quam vehicula elementum. Proin nunc purus, ultricies in lectus ut, congue rutrum eros. Sed quis tortor eu quam molestie eleifend ut tristique felis. Pellentesque non mi aliquet, viverra leo in, elementum erat. Morbi volutpat nisl id felis pellentesque, sit amet sodales orci tristique. Fusce sed accumsan odio. Praesent vel mi nunc. Nam euismod."
+        ],
+        [
+            "title" => "Post Kedua",
+            "slug" => "post-kedua",
+            "author" => "Hirai Momo",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus augue dolor, et feugiat justo commodo vel. In at augue in elit vehicula laoreet porta vitae odio. Mauris ultrices vestibulum est sed tempus. Fusce suscipit ipsum quis erat eleifend, pretium accumsan nulla faucibus. Duis congue faucibus volutpat. Sed at tortor tempor, imperdiet leo ut, semper lacus. Pellentesque suscipit enim at."
+        ]
+    ];
+
+    $new_post = [];
+    foreach($blog_post as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
