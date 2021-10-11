1aceb241a8e5b0ce28726804919215673d42f192
diasdigital <78405123+diasdigital@users.noreply.github.com>
2021-10-11 16:15:55 +0700

    Struktur Folder, Routes, & View
    Di video ini, kita diajarkan tentang cara menggunakan routes, membuat view, lalu memanggil view tersebut lewat function di routes.
    Dan juga mengirim data lewat routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Myoui Mina",
+        "email" => "mina@twice.com",
+        "image" => "mina.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
