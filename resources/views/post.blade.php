11bccbae344fdc876009570c78315d81ac457351
diasdigital <78405123+diasdigital@users.noreply.github.com>
2021-11-04 06:14:57 +0700

    Redesign UI
    Di video ini kita fokus dalam mempercantik tampilan web kita. Dimulai dari  membuat card untuk masing-masing post, hingga menambah gambar secara otomatis menggunakan API milik Unsplash.
    Kita juga memperbaiki seeder agar body postingan memiliki tag p

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.main')
 
 @section('konten')
-  <h2>{{ $post->title }}</h2>
 
-  <p>by: <a href="/author/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+  <div class="row justify-content-center mb-5">
+    <div class="col-md-8">
+      <h2 class="mb-3">{{ $post->title }}</h2> 
 
-  {!! $post->body !!}
+      <p>by: <a href="/author/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-  <a href="/posts" class="d-block mt-5">Back</a>
+      <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" class="img-fluid">
+
+      <article class="my-3 fs-5">
+        {!! $post->body !!}
+      </article>
+
+      <a href="/posts" class="d-block mt-5">Back</a>
+    </div>
+  </div>
+
+
+  
+
+  
 
 
 @endsection
