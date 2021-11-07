cece67e12046e2feb699e755d2fbbbde627a22e6
diasdigital <78405123+diasdigital@users.noreply.github.com>
2021-11-07 11:35:53 +0700

    User Registration
    Di video ini kita membuat fitur registrasi untuk user, beserta validasi, mencegah csrf, dan juga alert saat data berhasil diinput

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,5 @@ Route::get('/categories', function(){
 });
 
 Route::get('/login', [LoginController::class, 'index']);
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
