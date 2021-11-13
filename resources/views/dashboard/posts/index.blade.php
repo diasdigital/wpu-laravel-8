1c0e3db2539259024e36e3d2ab8bdf56dbfc144c
diasdigital <78405123+diasdigital@users.noreply.github.com>
2021-11-13 23:31:35 +0700

    Create Post Form
    Di video ini kita membuat form untuk unggah post, mulai dari titlenya yang teks biasa, slugnya yang dibikin otomatis menggunakan library eloquent sluggable, categorynya yang diquery dari table category, dan bodynya yang dibuat dengan library trix editor agar lebih mudah editnya

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -3,10 +3,11 @@
 @section('konten')
 
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
+    <h1 class="h2">My Posts</h1>
   </div>
 
   <div class="table-responsive">
+    <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
     <table class="table table-striped table-sm">
       <thead>
         <tr>
